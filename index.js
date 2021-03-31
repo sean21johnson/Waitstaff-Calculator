42413ed4945751f43787da7fb25effe401d56af4
Sean Johnson <seanjohnson220@gmail.com>
2021-03-31 11:53:18 -0400

    html code in

---
+++ index.js
@@ -0,0 +1,15 @@
+/* 
+Essentially need 3 sections overall
+    Enter the Meal Details
+        Base Meal Price and an input box where you can only input a number
+        Tax Rate % where you input a number from 0 to 100
+        Tip Percentage where you input a number greater than 0
+    Customer Charges
+        Subtotal which should be recording the combined amount of Base Meal Price and Tax Rate to total amount
+        Tip which should just calculate the the total tip amount for that meal
+        Total should should calculate the total cost of that meal
+    My Earnings Info
+        Tip Total which is going to be a cumulative running total of the tips earned
+        Meal count which should increase by 1 every time you hit the submit button
+        Average Tip Per Meal should just just take the running Tip Total and divide it by the running Meal Count
+*/
